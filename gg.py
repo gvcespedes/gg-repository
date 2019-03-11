53ee60ce52c57a356fcecf21b1d7d0f6a4f1df3e
gvcespedes <gvcespedes@uc.cl>
2019-03-11 10:19:03 -0300

    nuevo archivo

---
+++ gg.py
@@ -0,0 +1,12 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Users/geral/Desktop/gg.py ===================
+hello
+>>> 
+=================== RESTART: C:/Users/geral/Desktop/gg.py ===================
+hello
+>>> 
+=============== RESTART: C:\Users\geral\Downloads\5\Modelo.py ===============
+Changed value of parameter MIPGap to 1.0
+   Prev: 0.0001  Min: 0.0  Max: 1e+100  Default: 0.0001
